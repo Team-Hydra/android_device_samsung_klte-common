ca76f26845db390eda296b486976268ecdbb2bcf
Matt Filetto <matt.filetto@gmail.com>
2016-02-03 15:29:53 -0800

    kang hlte bluedroid
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -20,9 +20,9 @@
 
 #define BTM_DEF_LOCAL_NAME   "Samsung Galaxy S5"
 
-// skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  TRUE
-// Enables interleaved scan
-#define BTA_HOST_INTERLEAVE_SEARCH TRUE
+#define BTA_DISABLE_DELAY 100 /* in milliseconds */
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
+#define BLE_VND_INCLUDED TRUE
 
 #endif
